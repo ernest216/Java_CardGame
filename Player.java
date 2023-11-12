3822c6c1e11a00f39bbd483b7600d97edead1c0d
deschambone <150050119+deschambone@users.noreply.github.com>
2023-11-12 16:51:05 +0000

    Player.java

--- Player.java
+++ Player.java
@@ -3,30 +3,24 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Scanner;
+
+import javax.smartcardio.Card;
 
 public class Player{
     private int playerIndex;
     private ArrayList<Card> playerCards;
-    private Deck drawDeck;
-    private Deck discardDeck;
+    private CardDeck preferredDeck;
     private String playerOutput;
 
 
     public Player(int playerIndex) {
         this.playerIndex = playerIndex;
         this.playerCards = new ArrayList<>();
-
+        this.preferredDeck = decks.get(playerIndex - 1); // Assuming player indices start from 1
     }
-
-    /*public void setDrawDeck(Deck drawDeck) {
-        this.drawDeck = drawDeck;
-    }
-
-    public void setDiscardDeck(Deck discardDeck) {
-        this.discardDeck = discardDeck;
-    }*/
-
     /* Logs the current data to a file
      * @throws IOException if an error occurs while writing to the file
      */
@@ -57,24 +51,82 @@ public class Player{
         return this.playerCards;
     }
 
-    public void addCard(Card card) {
-        playerCards.add(card);
-    }
-
 
     /* */
     public void win(int winner) {
-        this.winner = win;
+        this.winner = winner;
     }
 
 
     /* */
     public int getWinner() {
-        return win;
+        return winner;
     }
 
+    /* Function that returna string of the values of cards in a player's hand 
+     * With format e.g. (1 2 3 4)
+    */
+    public String playerCardsTotal() {
+        String playerCardsTotal = "";
+        for (int i=0; i < getPlayerCards().size(); i++) {
+            playerCardsTotal += getPlayerCards().getValue() + " ";
+        }
+        return playerCardsTotal;
+    } 
 
+    /* Function that checks if player has a winning hand
+     * 
+     */
+    public void checkWin() throws IOException {
+        if (winner !=0) {
+            return;
+        }
+        Card n = playerCards.get(0);
+        int value = n.getValue();
+        for (int i = 1; i < 4; i++) {
+            if (value != playerCards.get(i).getValue()) {
+                return;
+            }
+        }
+
+        synchronized(this) {
+            System.out.printIn("Player " + playerIndex + " is the winner");
+            game.declareWin(playerIndex);
+            this.winner = playerIndex;
+        }
+    }
+
+    /* toString method to include player information */
+    public String toString() {
+        return "Player number is " + getPlayerIndex() + " with current hand " + getPlayerCards();
+    }
+
+    /* Synchronised method to draw/discard cards */
+    public synchronized void CardAction() throws IOException {
+        if (draw.getDeck().size() < 4) {
+            return;
+        }
+        ArrayList<Card> unwanted = new ArrayList<>();
+
+        for (int i=0; i<4; i++) {
+            card n = playerCards.get(i);
+            if (n.getValue() != playerIndex) {
+                unwanted.add(n);
+            }
+        }
+    
+    Collections.shuffle(unwanted);
+
+    Card cardToDiscard = unwanted.get(0);
+
+    disc.addCardDeck(cardToDiscard);
+
+    playerCards.remove(cardToDiscard);
+
+    String discMessage = "Player " + playerIndex + "discards " + cardToDiscard.getValue() + " to deck " + disc.getDeckNum();
+    logs += drawMessage + "\n"
+
+    String message = "Player " + playerIndex + "'s current hand is " + CardAction();
+    logs += message + "\n\n";
+    }
 }
-
-
-
